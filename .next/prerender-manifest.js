15d449c9986bf880b3a57c2129591ba3c881842a
Anku <psingh@netclues.in>
2024-12-03 18:56:13 +0530

    Fix component paths

---
+++ .next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"preview\":{\"previewModeId\":\"13bfe3b38631612f7b07f278da839054\",\"previewModeSigningKey\":\"68b3565e1eb5a98c099f5b6893c2d494021a6cb1c2b1b3fb0bc72d157716198c\",\"previewModeEncryptionKey\":\"a8dfed223b7a5202f2d553bcb3e708b0d6d8bede0862adf478eca467b17d5394\"}}"
