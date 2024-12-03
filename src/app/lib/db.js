e7e7d47105938edd52a8a3e0238aec7343758a95
Anku <psingh@netclues.in>
2024-12-03 18:44:08 +0530

    first commit

---
+++ src/app/lib/db.js
@@ -0,0 +1,3 @@
+const { username, password } = process.env;
+export const connectionStr =
+  "mongodb+srv://psingh:psinghmongo@user.rxyn8.mongodb.net/restoDB?retryWrites=true&w=majority&appName=Cluster0";
